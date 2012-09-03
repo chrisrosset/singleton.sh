b833d36b1c7b5b8c59e52c4475c3818f3817ed15
Christopher Rosset <k.rosset@gmail.com>
2012-09-03 19:44:15 +0100

    added a second (optional) parameter for the session name

--- singleton.sh
+++ singleton.sh
@@ -5,9 +5,6 @@
 # Runs a program in a tmux session. If the program is already running, the script
 # reattaches to the already running session.
 
-# global variables
-expected_args=1
-
 # Exit error codes
 e_args=1
 e_not_in_path=2
@@ -17,7 +14,8 @@ error()
 {
 	case $1 in
 	$e_args)
-		echo "Usage: singleton <program_name>"
+		echo "Usage: singleton <program_name> [session_name]"
+		echo "If not specified, session_name will be derived from program_name."
 		;;
 	$e_not_in_path)
 		echo 'Requested program not in $PATH.'
@@ -30,36 +28,45 @@ error()
 	exit $1
 }
 
-if [ $# -ne 1 ]
-then
+
+# global variables
+command="$1"
+session="$2"
+program=`echo $1 | cut -d" " -f 1`
+
+
+
+if [ $# -eq 2 ]; then
+	sleep 1
+elif [ $# -eq 1 ]; then
+	session="$program"
+else
 	error $e_args
 fi
 
 which tmux > /dev/null 2>&1
 prog_in_path=$?
 
-if [ $prog_in_path -ne 0 ]
-then
-	error $e_not_in_path
+if [ $prog_in_path -ne 0 ]; then
+	error $e_tmux_not_available
 fi
 
-which $1 > /dev/null 2>&1
+which $program > /dev/null 2>&1
 prog_in_path=$?
 
-if [ $prog_in_path -ne 0 ]
-then
+if [ $prog_in_path -ne 0 ]; then
 	error $e_not_in_path
 fi
 
 # get session list
-tmux has-session -t $1
+tmux has-session -t "$session"
 tmux_running=$?
 
 if [ $tmux_running -ne 0 ]
 then
 	# tmux server not running or session unavailable, run new session
-	tmux new-session -s $1 $1
+	tmux new-session -s "$session" "$command"
 else
 	# session running, reattach
-	tmux attach-session -t $1
+	tmux attach-session -t "$session"
 fi
